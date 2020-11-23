485e945e2627c6c3cc7799962af44a2a7024002b
Gregg <greggreedonline@gmail.com>
2020-11-22 21:17:09 -0500

    Add project files.

---
+++ CSharpAlgoData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpAlgoData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //~SELECTIONSORT~
+            //int[] arr = new int[] { 1, 3, 2 };
+            //int[] arr2;
+            //SelectionSort obj = new SelectionSort();
+            //arr2 = obj.SectionSortFunction<int>(arr);
+            //foreach (var i in arr2)
+            //{
+            //    Console.Write(i);
+            //}
+        }
+    }
+}
---
+++ CSharpAlgoData/SelectionSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAlgoData
+{
+    public class SelectionSort
+    {
+        //BIG-O = O(n^2)
+        //there are two loops, essentially
+        public T[] SectionSortFunction<T>(T[] arr) where T: IComparable
+        {
+            T[] newArr = arr;
+            for(int i = 0; i < newArr.Length-1; i++)
+            {
+                int minIndex = i;
+                T minValue = newArr[i];
+                for (int j = i + 1; j < newArr.Length; j++)
+                {
+                    //less than = comes before
+                    if (newArr[j].CompareTo(minValue) < 0) 
+                    {
+                        minIndex = j;
+                        minValue = newArr[j];
+                    }
+                }
+                T temp = newArr[i];
+                newArr[i] = newArr[minIndex];
+                newArr[minIndex] = temp;
+            }
+            return newArr;
+        }
+
+    }
+}
