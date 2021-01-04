9c0c03d147fa5881c9dea3c78bb1cd6d72aff600
Gregg <greggreedonline@gmail.com>
2021-01-03 22:33:41 -0500

    tree updates

---
+++ CSharpAlgoData/Trees/BinarySearchTree.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAlgoData.Trees
+{
+    //The presented implementation of the BST is based on the code shown at https://en.wikipedia.org/wiki/Binary_search_tree.
+    class BinarySearchTree
+    {
+    }
+
+    public class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; }
+    }
+
+    public class BinaryTreeNode<T> : TreeNode<T>
+    {
+        public BinaryTreeNode() => Children =
+        new List<TreeNode<T>>() { null, null };
+
+        public BinaryTreeNode<T> Parent { get; set; }
+
+        public BinaryTreeNode<T> Left
+        {
+            get { return (BinaryTreeNode<T>)Children[0]; }
+            set { Children[0] = value; }
+        }
+
+        public BinaryTreeNode<T> Right
+        {
+            get { return (BinaryTreeNode<T>)Children[1]; }
+            set { Children[1] = value; }
+        }
+
+        public int GetHeight()
+        {
+            int height = 1;
+            BinaryTreeNode<T> current = this;
+            while (current.Parent != null)
+            {
+                height++;
+                current = current.Parent;
+            }
+            return height;
+        }
+    }
+    public class BinaryTree<T>
+    {
+        public BinaryTreeNode<T> Root { get; set; }
+        public int Count { get; set; }
+    }
+    public class BinarySearchTree<T> : BinaryTree<T> where T : IComparable
+    {
+        public bool Contains(T data)
+        {
+            BinaryTreeNode<T> node = Root;
+            while (node != null)
+            {
+                int result = data.CompareTo(node.Data);
+                if (result == 0)
+                {
+                    return true;
+                }
+                else if (result < 0)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    node = node.Right;
+                }
+            }
+            return false;
+        }
+        public void Add(T data)
+        {
+            BinaryTreeNode<T> parent = GetParentForNewNode(data);
+            BinaryTreeNode<T> node = new BinaryTreeNode<T>()
+            { Data = data, Parent = parent };
+
+            if (parent == null)
+            {
+                Root = node;
+            }
+            else if (data.CompareTo(parent.Data) < 0)
+            {
+                parent.Left = node;
+            }
+            else
+            {
+                parent.Right = node;
+            }
+
+            Count++;
+        }
+        private BinaryTreeNode<T> GetParentForNewNode(T data)
+        {
+            BinaryTreeNode<T> current = Root;
+            BinaryTreeNode<T> parent = null;
+            while (current != null)
+            {
+                parent = current;
+                int result = data.CompareTo(current.Data);
+                if (result == 0)
+                {
+                    throw new ArgumentException(
+                        $"The node {data} already exists.");
+                }
+                else if (result < 0)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+                }
+            }
+
+            return parent;
+        }
+        public void Remove(T data)
+        {
+            Remove(Root, data);
+        }
+        private void Remove(BinaryTreeNode<T> node, T data)
+        {
+            if (node == null)
+            {
+                throw new ArgumentException($"The node {data} does not exist.");
+            }
+            else if (data.CompareTo(node.Data) < 0)
+            {
+                Remove(node.Left, data);
+            }
+            else if (data.CompareTo(node.Data) > 0)
+            {
+                Remove(node.Right, data);
+            }
+            else
+            {
+                if (node.Left == null && node.Right == null)
+                {
+                    ReplaceInParent(node, null);
+                    Count--;
+                }
+                else if (node.Right == null)
+                {
+                    ReplaceInParent(node, node.Left);
+                    Count--;
+                }
+                else if (node.Left == null)
+                {
+                    ReplaceInParent(node, node.Right);
+                    Count--;
+                }
+                else
+                {
+                    BinaryTreeNode<T> successor = FindMinimumInSubtree(node.Right);
+                    node.Data = successor.Data;
+                    Remove(successor, successor.Data);
+                }
+            }
+        }
+        private void ReplaceInParent(BinaryTreeNode<T> node, BinaryTreeNode<T> newNode)
+        {
+            if (node.Parent != null)
+            {
+                if (node.Parent.Left == node)
+                {
+                    node.Parent.Left = newNode;
+                }
+                else
+                {
+                    node.Parent.Right = newNode;
+                }
+            }
+            else
+            {
+                Root = newNode;
+            }
+
+            if (newNode != null)
+            {
+                newNode.Parent = node.Parent;
+            }
+        }
+        private BinaryTreeNode<T> FindMinimumInSubtree(BinaryTreeNode<T> node)
+        {
+            while (node.Left != null)
+            {
+                node = node.Left;
+            }
+            return node;
+        }
+    }
+}
--- CSharpAlgoData/Trees/BinaryTree.cs
+++ CSharpAlgoData/Trees/BinaryTree.cs
@@ -1,194 +1,194 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace CSharpAlgoData.Trees
-{
-    class BinaryTree
-    {
-    }
+//namespace CSharpAlgoData.Trees
+//{
+//    class BinaryTree
+//    {
+//    }
 
-    public class BinaryTreeNode<T> : TreeNode<T> //in basic tree
-    {
-        public BinaryTreeNode() => Children =
-            new List<TreeNode<T>>() { null, null };
+//    public class BinaryTreeNode<T> : TreeNode<T> //in basic tree
+//    {
+//        public BinaryTreeNode() => Children =
+//            new List<TreeNode<T>>() { null, null };
 
-        public BinaryTreeNode<T> Left
-        {
-            get { return (BinaryTreeNode<T>)Children[0]; }
-            set { Children[0] = value; }
-        }
+//        public BinaryTreeNode<T> Left
+//        {
+//            get { return (BinaryTreeNode<T>)Children[0]; }
+//            set { Children[0] = value; }
+//        }
 
-        public BinaryTreeNode<T> Right
-        {
-            get { return (BinaryTreeNode<T>)Children[1]; }
-            set { Children[1] = value; }
-        }
-    }
-    public class BinaryTree<T>
-    {
-        public enum TraversalEnum
-        {
-            PREORDER,
-            INORDER,
-            POSTORDER
-        }
-        public BinaryTreeNode<T> Root { get; set; }
-        public int Count { get; set; }
+//        public BinaryTreeNode<T> Right
+//        {
+//            get { return (BinaryTreeNode<T>)Children[1]; }
+//            set { Children[1] = value; }
+//        }
+//    }
+//    public class BinaryTree<T>
+//    {
+//        public enum TraversalEnum
+//        {
+//            PREORDER,
+//            INORDER,
+//            POSTORDER
+//        }
+//        public BinaryTreeNode<T> Root { get; set; }
+//        public int Count { get; set; }
 
-        private void TraversePreOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
-        {
-            if (node != null)
-            {
-                result.Add(node);
-                TraversePreOrder(node.Left, result);
-                TraversePreOrder(node.Right, result);
-            }
-        }
+//        private void TraversePreOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
+//        {
+//            if (node != null)
+//            {
+//                result.Add(node);
+//                TraversePreOrder(node.Left, result);
+//                TraversePreOrder(node.Right, result);
+//            }
+//        }
 
-        private void TraverseInOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
-        {
-            if (node != null)
-            {
-                TraverseInOrder(node.Left, result);
-                result.Add(node);
-                TraverseInOrder(node.Right, result);
-            }
-        }
+//        private void TraverseInOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
+//        {
+//            if (node != null)
+//            {
+//                TraverseInOrder(node.Left, result);
+//                result.Add(node);
+//                TraverseInOrder(node.Right, result);
+//            }
+//        }
 
-        private void TraversePostOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
-        {
-            if (node != null)
-            {
-                TraversePostOrder(node.Left, result);
-                TraversePostOrder(node.Right, result);
-                result.Add(node);
-            }
-        }
+//        private void TraversePostOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
+//        {
+//            if (node != null)
+//            {
+//                TraversePostOrder(node.Left, result);
+//                TraversePostOrder(node.Right, result);
+//                result.Add(node);
+//            }
+//        }
 
-        public List<BinaryTreeNode<T>> Traverse(TraversalEnum mode)
-        {
-            List<BinaryTreeNode<T>> nodes = new List<BinaryTreeNode<T>>();
-            switch (mode)
-            {
-                case TraversalEnum.PREORDER:
-                TraversePreOrder(Root, nodes);
-                break;
-                case TraversalEnum.INORDER:
-                TraverseInOrder(Root, nodes);
-                break;
-                case TraversalEnum.POSTORDER:
-                TraversePostOrder(Root, nodes);
-                break;
-            }
-            return nodes;
-        }
+//        public List<BinaryTreeNode<T>> Traverse(TraversalEnum mode)
+//        {
+//            List<BinaryTreeNode<T>> nodes = new List<BinaryTreeNode<T>>();
+//            switch (mode)
+//            {
+//                case TraversalEnum.PREORDER:
+//                TraversePreOrder(Root, nodes);
+//                break;
+//                case TraversalEnum.INORDER:
+//                TraverseInOrder(Root, nodes);
+//                break;
+//                case TraversalEnum.POSTORDER:
+//                TraversePostOrder(Root, nodes);
+//                break;
+//            }
+//            return nodes;
+//        }
 
-        public int GetHeight()
-        {
-            int height = 0;
-            foreach (BinaryTreeNode<T> node
-                in Traverse(TraversalEnum.PREORDER))
-            {
-                height = Math.Max(height, node.GetHeight());
-            }
-            return height;
-        }
-    }
+//        public int GetHeight()
+//        {
+//            int height = 0;
+//            foreach (BinaryTreeNode<T> node
+//                in Traverse(TraversalEnum.PREORDER))
+//            {
+//                height = Math.Max(height, node.GetHeight());
+//            }
+//            return height;
+//        }
+//    }
 
-    public class QuizItem
-    {
-        public string Text { get; set; }
-        public QuizItem(string text) => Text = text;
-    }
+//    public class QuizItem
+//    {
+//        public string Text { get; set; }
+//        public QuizItem(string text) => Text = text;
+//    }
 
-    public class QuizItemImplementation
-    {
-        public QuizItemImplementation()
-        {
-            BinaryTree<QuizItem> tree = GetTree();
-            BinaryTreeNode<QuizItem> node = tree.Root;
-            while (node != null)
-            {
-                if (node.Left != null || node.Right != null)
-                {
-                    Console.Write(node.Data.Text);
-                    switch (Console.ReadKey(true).Key)
-                    {
-                        case ConsoleKey.Y:
-                        WriteAnswer(" Yes");
-                        node = node.Left;
-                        break;
-                        case ConsoleKey.N:
-                        WriteAnswer(" No");
-                        node = node.Right;
-                        break;
-                    }
-                }
-                else
-                {
-                    WriteAnswer(node.Data.Text);
-                    node = null;
-                }
-            }
+//    public class QuizItemImplementation
+//    {
+//        public QuizItemImplementation()
+//        {
+//            BinaryTree<QuizItem> tree = GetTree();
+//            BinaryTreeNode<QuizItem> node = tree.Root;
+//            while (node != null)
+//            {
+//                if (node.Left != null || node.Right != null)
+//                {
+//                    Console.Write(node.Data.Text);
+//                    switch (Console.ReadKey(true).Key)
+//                    {
+//                        case ConsoleKey.Y:
+//                        WriteAnswer(" Yes");
+//                        node = node.Left;
+//                        break;
+//                        case ConsoleKey.N:
+//                        WriteAnswer(" No");
+//                        node = node.Right;
+//                        break;
+//                    }
+//                }
+//                else
+//                {
+//                    WriteAnswer(node.Data.Text);
+//                    node = null;
+//                }
+//            }
 
-        }
+//        }
 
-        private static BinaryTree<QuizItem> GetTree()
-        {
-            BinaryTree<QuizItem> tree = new BinaryTree<QuizItem>();
-            tree.Root = new BinaryTreeNode<QuizItem>()
-            {
-                Data = new QuizItem("Do you have experience in developingapplications ? "),
+//        private static BinaryTree<QuizItem> GetTree()
+//        {
+//            BinaryTree<QuizItem> tree = new BinaryTree<QuizItem>();
+//            tree.Root = new BinaryTreeNode<QuizItem>()
+//            {
+//                Data = new QuizItem("Do you have experience in developingapplications ? "),
 
-                Children = new List<TreeNode<QuizItem>>()
-                {
-                    new BinaryTreeNode<QuizItem>()
-                    {
-                        Data = new QuizItem("Have you worked as a developer for more than 5 years ? "),
-                        Children = new List<TreeNode<QuizItem>>()
-                        {
-                            new BinaryTreeNode<QuizItem>() {Data = new QuizItem("Apply as a senior developer!") },
-                            new BinaryTreeNode<QuizItem>() {Data = new QuizItem("Apply as a middle developer!") }
-                        }
-                    },
-                    new BinaryTreeNode<QuizItem>()
-                    {
-                        Data = new QuizItem("Have you completed the university?"),
-                        Children = new List<TreeNode<QuizItem>>()
-                        {
-                            new BinaryTreeNode<QuizItem>()
-                            {
-                                Data = new QuizItem("Apply for a junior developer!")
-                            },
-                            new BinaryTreeNode<QuizItem>()
-                            {
-                                Data = new QuizItem("Will you find some time during the semester?"),
-                                Children = new List<TreeNode<QuizItem>>()
-                                {
-                                    new BinaryTreeNode<QuizItem>()
-                                    {
-                                        Data = new QuizItem("Apply for our  long-time internship program!")
-                                    },
-                                    new BinaryTreeNode<QuizItem>()
-                                    {
-                                        Data = new QuizItem("Apply forsummer internship program!")
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            };
-            tree.Count = 9;
-            return tree;
-        }
-        private static void WriteAnswer(string text)
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(text);
-            Console.ForegroundColor = ConsoleColor.Gray;
-        }
+//                Children = new List<TreeNode<QuizItem>>()
+//                {
+//                    new BinaryTreeNode<QuizItem>()
+//                    {
+//                        Data = new QuizItem("Have you worked as a developer for more than 5 years ? "),
+//                        Children = new List<TreeNode<QuizItem>>()
+//                        {
+//                            new BinaryTreeNode<QuizItem>() {Data = new QuizItem("Apply as a senior developer!") },
+//                            new BinaryTreeNode<QuizItem>() {Data = new QuizItem("Apply as a middle developer!") }
+//                        }
+//                    },
+//                    new BinaryTreeNode<QuizItem>()
+//                    {
+//                        Data = new QuizItem("Have you completed the university?"),
+//                        Children = new List<TreeNode<QuizItem>>()
+//                        {
+//                            new BinaryTreeNode<QuizItem>()
+//                            {
+//                                Data = new QuizItem("Apply for a junior developer!")
+//                            },
+//                            new BinaryTreeNode<QuizItem>()
+//                            {
+//                                Data = new QuizItem("Will you find some time during the semester?"),
+//                                Children = new List<TreeNode<QuizItem>>()
+//                                {
+//                                    new BinaryTreeNode<QuizItem>()
+//                                    {
+//                                        Data = new QuizItem("Apply for our  long-time internship program!")
+//                                    },
+//                                    new BinaryTreeNode<QuizItem>()
+//                                    {
+//                                        Data = new QuizItem("Apply forsummer internship program!")
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            };
+//            tree.Count = 9;
+//            return tree;
+//        }
+//        private static void WriteAnswer(string text)
+//        {
+//            Console.ForegroundColor = ConsoleColor.White;
+//            Console.WriteLine(text);
+//            Console.ForegroundColor = ConsoleColor.Gray;
+//        }
 
-    }
-}
+//    }
+//}
