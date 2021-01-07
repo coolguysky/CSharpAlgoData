d648ff0b5f4992b58e9fc7688b9fb3b201868f11
Gregg <greggreedonline@gmail.com>
2021-01-07 13:43:15 -0500

    bheap and fi heap

---
+++ CSharpAlgoData/Trees and Heaps/BinaryHeap.cs
@@ -0,0 +1,25 @@
+﻿using PommaLabs.Hippie;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAlgoData.Trees_and_Heaps
+{
+    class BinaryHeap
+    {
+        public BinaryHeap()
+        {
+            List<int> unsorted = new List<int>() { 50, 33, 78, -23, 90, 41 };
+            MultiHeap<int> heap = HeapFactory.NewBinaryHeap<int>();
+            unsorted.ForEach(i => heap.Add(i));
+            Console.WriteLine("Unsorted: " + string.Join(", ", unsorted));
+
+            List<int> sorted = new List<int>(heap.Count);
+            while (heap.Count > 0)
+            {
+                sorted.Add(heap.RemoveMin());
+            }
+            Console.WriteLine("Sorted: " + string.Join(", ", sorted));
+        }
+    }
+}
---
+++ CSharpAlgoData/Trees and Heaps/FibonacciHeap.cs
@@ -0,0 +1,26 @@
+﻿using PommaLabs.Hippie;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAlgoData.Trees_and_Heaps
+{
+    class FibonacciHeap
+    {
+
+        public FibonacciHeap()
+        {
+            List<int> unsorted = new List<int>() { 50, 33, 78, -23, 90, 41 };
+            MultiHeap<int> heap = HeapFactory.NewFibonacciHeap<int>();
+            unsorted.ForEach(i => heap.Add(i));
+            Console.WriteLine("Unsorted: " + string.Join(", ", unsorted));
+
+            List<int> sorted = new List<int>(heap.Count);
+            while (heap.Count > 0)
+            {
+                sorted.Add(heap.RemoveMin());
+            }
+            Console.WriteLine("Sorted: " + string.Join(", ", sorted));
+        }
+    }
+}
